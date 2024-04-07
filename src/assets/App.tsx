246573be768faafec5dbcae9837e4d6b4ee90570
robin <robin.dm@outlook.fr>
2024-04-07 23:41:26 +0200

    Update App.tsx

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
   const [statuses, setStatuses] = useState<Array<StatusesInterface>>();
 
   const fetchStatuses = async () => {
-    let statusesReq = await fetch("/statuses", {
+    let statusesReq = await fetch("api/statuses", {
       mode: "cors",
     });
     setStatuses(await statusesReq.json());
