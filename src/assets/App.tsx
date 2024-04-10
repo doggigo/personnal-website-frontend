e46255cfd683f7bb67d85cc43a6004bd2549887f
robin <robin.dm@outlook.fr>
2024-04-10 21:27:35 +0200

    Readme

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -28,12 +28,6 @@ export default function App() {
     <div id="app">
       <Profile />
       <div id="statuses">
-        <Status
-          key={1}
-          date={1}
-          title="HEY"
-          content="HEY &blue HEY &white HEYYY &rainbow sava {https://images.ctfassets.net/denf86kkcx7r/2M8VfjD3l28336lmz4eatI/ab9251e30102058a16d2e7c538a098b4/top_5_chien_intelligents_vid_o}"
-        />
         {statuses?.map((status) => (
           <Status
             key={status.id}
